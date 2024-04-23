c3c76df48111af9c01f38d1eb278957ab8d21a1b
DESKTOP-D179MQQ\USER <ckswhd6868@naver.com>
2024-04-23 16:53:05 +0900

    학생 조회 기능 종합

--- spring4/src/main/java/com/spring4/controller/StudentController.java
+++ spring4/src/main/java/com/spring4/controller/StudentController.java
@@ -3,8 +3,10 @@ package com.spring4.controller;
 import com.spring4.mapper.StudentMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class StudentController {
@@ -15,5 +17,51 @@ public class StudentController {
     public String list(Model model) {
         model.addAttribute("students", studentMapper.findAll());
         return "student/list";
+    }
+
+    @RequestMapping("student/list1")
+    public String list1(Model model, String srchText) {
+        if (srchText == null) srchText = "";
+        model.addAttribute("students", studentMapper.findByName(srchText + "%"));
+        model.addAttribute("srchText", srchText);
+        return "student/list1";
+    }
+
+    // http://localhost:8088/student/detail/7 이런식으로 검색 /번호
+    @RequestMapping("student/detail")
+    public String detail(Model model, @RequestParam("id") Integer id) {
+        if (id == null) id = 4;
+        model.addAttribute("student", studentMapper.findById(id));
+        return "student/detail";
+    }
+
+    
+    // http://localhost:8088/student/detail?id=7 이런식으로 검색 ?id=번호
+    @RequestMapping("student/detail/{id}")
+    public String detail2(Model model, @PathVariable Integer id) {
+        if (id == null) id = 4;
+        model.addAttribute("student", studentMapper.findById(id));
+        return "student/detail";
+    }
+
+    @RequestMapping("student/test1")
+    public String test1(Model model) {
+        return "student/test1";
+    }
+
+    @RequestMapping("student/list2")
+    public String list2(Model model, String srchText) {
+        if (srchText == null) srchText = "";
+        model.addAttribute("students", studentMapper.findByName(srchText + "%"));
+        model.addAttribute("srchText", srchText);
+        return "student/list2";
+    }
+
+    @RequestMapping("student/list3")
+    public String list3(Model model, String srchText) {
+        if (srchText == null) srchText = "";
+        model.addAttribute("students", studentMapper.findByName(srchText + "%"));
+        model.addAttribute("srchText", srchText);
+        return "student/list3";
     }
 }
