1e905c4bfaa4feb5a7c77f4c09eccdae77cd0502
DESKTOP-D179MQQ\USER <ckswhd6868@naver.com>
2024-04-23 13:10:43 +0900

    Slf4j 어노테이션 사용법 - log.info

--- spring4/src/main/java/com/spring4/controller/BookController.java
+++ spring4/src/main/java/com/spring4/controller/BookController.java
@@ -1,11 +1,13 @@
 package com.spring4.controller;
 
 import com.spring4.mapper.BookMapper;
+// import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+// @Slf4j
 @Controller
 public class BookController {
     @Autowired
@@ -14,8 +16,9 @@ public class BookController {
     @RequestMapping("student/bookList")
     public String list(Model model){
         // 아래에서 book이 mapper에 있는데이터들을 이어 받고 jsp 파일에서
-        //<c:forEach var="book" items="${ book }"> items로 값을 넘겨줌
+        // <c:forEach var="book" items="${ book }"> items로 값을 넘겨줌
         model.addAttribute("book", bookMapper.findAll());
+        // log.info(bookMapper.findAll());
         return "student/bookList";
     }
 }
