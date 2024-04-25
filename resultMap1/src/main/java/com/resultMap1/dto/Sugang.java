7f43e84abd555cbbe4a2df79ed89cf6f0bafb5a0
DESKTOP-D179MQQ\USER <ckswhd6868@naver.com>
2024-04-25 10:19:01 +0900

    Mybatis resultMap

---
+++ resultMap1/src/main/java/com/resultMap1/dto/Sugang.java
@@ -0,0 +1,20 @@
+package com.resultMap1.dto;
+
+import lombok.Data;
+
+@Data
+public class Sugang {
+    int id;
+    int lectureId;
+    int studentId;
+    boolean repeated;
+    boolean cancel;
+    String grade;
+
+    String studentNo;
+    String name;
+
+    String title;
+    int year;
+    String semester;
+}
