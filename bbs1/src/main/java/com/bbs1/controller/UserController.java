d06aaa11bf6662b74c2336a4e2d28a8f9ff18034
DESKTOP-D179MQQ\USER <ckswhd6868@naver.com>
2024-05-08 17:10:41 +0900

    의존성 주입 방법

--- bbs1/src/main/java/com/bbs1/controller/UserController.java
+++ bbs1/src/main/java/com/bbs1/controller/UserController.java
@@ -4,6 +4,7 @@ import com.bbs1.model.Pagination;
 import com.bbs1.model.UserEdit;
 import com.bbs1.service.UserService;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.annotation.Secured;
@@ -19,9 +20,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("user")
 @Secured("ROLE_ADMIN")
+// @RequiredArgsConstructor
 public class UserController {
+
+    // 의존성 주입
+    // 필드주입
     @Autowired
     UserService userService;
+
+
+    // 생성자 주입
+    // 위에를 이렇게 해서 의존성을 주입하거나
+    // private final UserService userService;
+    //
+    // public UserController(UserService userService) {
+    //     this.userService = userService;
+    // }
+
+    // 생성자 주입
+    // @RequiredArgsConstructor 을 사용하고 아래 코드를 써서 의존성을 주입하는 방법이 있다
+    // private final UserService userService;
 
     @Secured("ROLE_ADMIN")
     @GetMapping("list")
