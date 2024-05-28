2b78c8a925844cacac7f39489ecc41d2feb59716
DESKTOP-D179MQQ\USER <ckswhd6868@naver.com>
2024-05-28 10:02:53 +0900

    메인 페이지, 회원가입 페이지

---
+++ shop2/src/main/java/com/shop2/entity/Member.java
@@ -0,0 +1,48 @@
+package com.shop2.entity;
+
+import com.shop2.constant.Role;
+import com.shop2.dto.MemberFormDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Entity
+@Table(name="member")
+@Getter
+@Setter
+@ToString
+public class Member {
+
+    @Id
+    @Column(name="member_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private String address;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    public static Member createMember(MemberFormDto memberFormDto, PasswordEncoder passwordEncoder){
+        Member member = new Member(); // 이거를 생성 안하고 바로 this를 이용해서
+        member.setName(memberFormDto.getName()); // this.name = memverFormDto.getName() 이렇게 해도됨
+        member.setEmail(memberFormDto.getEmail());
+        member.setAddress(memberFormDto.getAddress());
+
+        // 비밀번호 암호화
+        String password = passwordEncoder.encode(memberFormDto.getPassword());
+        member.setPassword(password);
+        member.setRole(Role.ADMIN);
+        return member;
+    }
+
+}
