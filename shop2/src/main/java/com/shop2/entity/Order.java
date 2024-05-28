a0efe9ad01ff135d41ff344640f5c2f19b47ab1b
DESKTOP-D179MQQ\USER <ckswhd6868@naver.com>
2024-05-28 16:22:25 +0900

    영속성 전이

--- shop2/src/main/java/com/shop2/entity/Order.java
+++ shop2/src/main/java/com/shop2/entity/Order.java
@@ -29,7 +29,10 @@ public class Order {
     @Enumerated(EnumType.STRING)
     private OrderStatus orderStatus; //주문상태
 
-    @OneToMany(mappedBy = "order")
+    // CascadeType.ALL: 이 값은 모든 연쇄 삭제 작업을 수행
+    // 즉, 연관된 객체가 삭제되면 해당 객체와 연관된 모든 객체도 함께 삭제
+    // CascadeType.REMOVE: 이 값은 연관된 객체가 삭제될 때 해당 객체만 삭제하고, 연관된 객체는 삭제하지 않음
+    @OneToMany(mappedBy = "order",cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
     private LocalDateTime regTime;
