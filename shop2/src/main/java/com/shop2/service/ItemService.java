5d766aa941f4aede6e236cc9b5f71d0f52c8cd75
DESKTOP-D179MQQ\USER <ckswhd6868@naver.com>
2024-05-29 16:06:38 +0900

    상품 수정하기

--- shop2/src/main/java/com/shop2/service/ItemService.java
+++ shop2/src/main/java/com/shop2/service/ItemService.java
@@ -1,15 +1,18 @@
 package com.shop2.service;
 
 import com.shop2.dto.ItemFormDto;
+import com.shop2.dto.ItemImgDto;
 import com.shop2.entity.Item;
 import com.shop2.entity.ItemImg;
 import com.shop2.repository.ItemImgRepository;
 import com.shop2.repository.ItemRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -38,6 +41,42 @@ public class ItemService {
                 itemImg.setRepimgYn("N");
 
             itemImgService.saveItemImg(itemImg, itemImgFileList.get(i));
+        }
+        return item.getId();
+    }
+
+    @Transactional(readOnly = true) // 더티체킹을 안하기 위해서 readOnly = true
+    public ItemFormDto getItemDtl(Long itemId){
+        List<ItemImg> itemImgList = itemImgRepository.findByItemIdOrderByIdAsc(itemId);
+        List<ItemImgDto> itemImgDtoList = new ArrayList<>();
+
+        // entity -> dto
+        for (ItemImg itemImg : itemImgList) {
+            ItemImgDto itemImgDto = ItemImgDto.of(itemImg);
+            itemImgDtoList.add(itemImgDto);
+        }
+
+        Item item = itemRepository.findById(itemId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        // item정보와 image정보
+        ItemFormDto itemFormDto = ItemFormDto.of(item);
+        itemFormDto.setItemImgDtoList(itemImgDtoList);
+
+        return itemFormDto;
+    }
+
+    public Long updateItem(ItemFormDto itemFormDto, List<MultipartFile> itemImgFileList) throws Exception{
+        //상품 수정
+        Item item = itemRepository.findById(itemFormDto.getId())
+                .orElseThrow(EntityNotFoundException::new);
+        item.updateItem(itemFormDto);
+        List<Long> itemImgIds = itemFormDto.getItemImgIds();
+
+        //이미지 등록
+        for(int i=0;i<itemImgFileList.size();i++){
+            itemImgService.updateItemImg(itemImgIds.get(i),
+                    itemImgFileList.get(i));
         }
         return item.getId();
     }
