f54f9e44b42980b2986500be0c610533505ae7ac
DESKTOP-D179MQQ\USER <ckswhd6868@naver.com>
2024-05-30 18:07:10 +0900

    장바구니 기능

---
+++ shop2/src/main/java/com/shop2/service/CartService.java
@@ -0,0 +1,51 @@
+package com.shop2.service;
+
+import com.shop2.dto.CartItemDto;
+import com.shop2.entity.Cart;
+import com.shop2.entity.CartItem;
+import com.shop2.entity.Item;
+import com.shop2.entity.Member;
+import com.shop2.repository.CartItemRepository;
+import com.shop2.repository.CartRepository;
+import com.shop2.repository.ItemRepository;
+import com.shop2.repository.MemberRepository;
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Transactional
+public class CartService {
+
+    private final ItemRepository itemRepository;
+    private final MemberRepository memberRepository;
+    private final CartRepository cartRepository;
+    private final CartItemRepository cartItemRepository;
+    private final OrderService orderService;
+
+    public Long addCart(CartItemDto cartItemDto, String email) {
+
+        Item item = itemRepository.findById(cartItemDto.getItemId())
+                .orElseThrow(EntityNotFoundException::new);
+        Member member = memberRepository.findByEmail(email);
+
+        Cart cart = cartRepository.findByMemberId(member.getId());
+        if (cart == null) {
+            cart = Cart.createCart(member);
+            cartRepository.save(cart);
+        }
+
+        CartItem savedCartItem = cartItemRepository.findByCartIdAndItemId(cart.getId(), item.getId());
+
+        if (savedCartItem != null) {
+            savedCartItem.addCount(cartItemDto.getCount());
+            return savedCartItem.getId();
+        } else {
+            CartItem cartItem = CartItem.createCartItem(cart, item, cartItemDto.getCount());
+            cartItemRepository.save(cartItem);
+            return cartItem.getId();
+        }
+    }
+}
