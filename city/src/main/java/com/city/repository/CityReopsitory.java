35e618a926ab7590a66eee7649f6e0d7fdae3575
DESKTOP-D179MQQ\USER <ckswhd6868@naver.com>
2024-06-12 15:14:02 +0900

    접근 방법 주석 추가

--- city/src/main/java/com/city/repository/CityReopsitory.java
+++ city/src/main/java/com/city/repository/CityReopsitory.java
@@ -11,6 +11,8 @@ public interface CityReopsitory extends JpaRepository<City, Integer> {
     List<City> findAll();
     City findByName(String name);
 
+    // District엔티티로 접근하는 방법은 -> DistrictDistrictName
+    // District에 DistrictName으로 접근한다는 뜻
     Page<City> findByNameStartsWithOrDistrictDistrictNameStartsWith(
             String name, String districtDistrictName,Pageable pageable
     );
