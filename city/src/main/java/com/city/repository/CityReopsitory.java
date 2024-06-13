c803a99ef7b325879539dffaca9a35b5051524a3
DESKTOP-D179MQQ\USER <ckswhd6868@naver.com>
2024-06-13 11:58:33 +0900

    쿼리문으로 풀어서 검색기능 구현하는 방법

--- city/src/main/java/com/city/repository/CityReopsitory.java
+++ city/src/main/java/com/city/repository/CityReopsitory.java
@@ -4,6 +4,8 @@ import com.city.entity.City;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -16,4 +18,12 @@ public interface CityReopsitory extends JpaRepository<City, Integer> {
     Page<City> findByNameStartsWithOrDistrictDistrictNameStartsWith(
             String name, String districtDistrictName,Pageable pageable
     );
+
+    // 위에를 아래 쿼리로 풀어써서 해도됨
+    // like :name은 파라미터 String name 여기 name
+    @Query(value = "select c.id,c.name, c.area,c.population, c.district_id " +
+            "from city c join district on c.district_id = district.id where c.name like :name% " +
+            "or district.district_name like :districtName%", nativeQuery = true)
+    Page<City> 도시명과지역명검색(@Param("name") String name, @Param("districtName") String districtName, Pageable pageable);
+
 }
